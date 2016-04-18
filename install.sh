39487be1653d1f8fdcbd2e95260a21efe056d446
Artem Semenov <artem@semenov.ws>
2016-04-18 13:34:14 +0300

    Install script make files in ~/

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
-cat <<EOT > ~/.bash_profile
+cat > ~/.bash_profile << "EOF"
 #!/bin/bash
 
 load_bash_profile_files() {
     declare -a files=(
-        \$HOME/.bashrc.d/bash_colors # Custom color
-        \$HOME/.bashrc.d/bash_prompt # Custom bash prompt
-        \$(brew --prefix)/etc/bash_completion # Bash completion (installed via Homebrew)
+        $HOME/.bashrc.d/bash_colors # Custom color
+        $HOME/.bashrc.d/bash_prompt # Custom bash prompt
+        $(brew --prefix)/etc/bash_completion # Bash completion (installed via Homebrew)
     )
 
     # if these files are readable, source them
-    for index in \${!files[*]}
+    for index in ${!files[*]}
     do
-        if [[ -r \${files[\$index]} ]]; then
-            source \${files[\$index]}
+        if [[ -r ${files[$index]} ]]; then
+            source ${files[$index]}
         fi
     done
 }
@@ -21,28 +21,24 @@ load_bash_profile_files
 unset load_bash_profile_files
 
 if [ -f ~/.bashrc ]; then source ~/.bashrc; fi
-EOT
+EOF
 
-cat <<EOT > ~/.bashrc
+cat > ~/.bashrc << "EOF"
 #!/bin/bash
-
-if [ -z "\$PS1" ]; then
-   return
-fi
 
 load_bashrc_files() {
     declare -a files=(
-        \$HOME/.bashrc.d/bash_options # Options
-        \$HOME/.bashrc.d/bash_exports # Exports
-        \$HOME/.bashrc.d/functions/* # Functions
-        \$HOME/.bash_profile.local # Local and private settings not under version control (e.g. git credentials)
+        $HOME/.bashrc.d/bash_options # Options
+        $HOME/.bashrc.d/bash_exports # Exports
+        $HOME/.bashrc.d/functions/* # Functions
+        $HOME/.bash_profile.local # Local and private settings not under version control (e.g. git credentials)
     )
 
     # if these files are readable, source them
-    for index in \${!files[*]}
+    for index in ${!files[*]}
     do
-        if [[ -r \${files[\$index]} ]]; then
-            source \${files[\$index]}
+        if [[ -r ${files[$index]} ]]; then
+            source ${files[$index]}
         fi
     done
 }
@@ -51,33 +47,33 @@ load_bashrc_files
 unset load_bashrc_files
 
 if [ -f ~/.profile ]; then source ~/.profile; fi
-EOT
+EOF
 
-cat <<EOT > ~/.profile
+cat > ~/.profile << "EOF"
 #!/bin/bash
 
 # pathhelper from Apple default /private/etc/bashrc, plus empty PATH
 # as per http://www.softec.lu/site/DevelopersCorner/MasteringThePathHelper
 if [ -x /usr/libexec/path_helper ]; then
 	PATH=""
-	eval \`/usr/libexec/path_helper -s\`
+	eval `/usr/libexec/path_helper -s`
 fi
 
 load_profile_files() {
     declare -a files=(
-        \$HOME/.bashrc.d/bash_paths # Path modifications
-        \$HOME/.bashrc.d/bash_aliases # Aliases
+        $HOME/.bashrc.d/bash_paths # Path modifications
+        $HOME/.bashrc.d/bash_aliases # Aliases
     )
 
     # if these files are readable, source them
-    for index in \${!files[*]}
+    for index in ${!files[*]}
     do
-        if [[ -r \${files[\$index]} ]]; then
-            source \${files[\$index]}
+        if [[ -r ${files[$index]} ]]; then
+            source ${files[$index]}
         fi
     done
 }
 
 load_profile_files
 unset load_profile_files
-EOT
+EOF
